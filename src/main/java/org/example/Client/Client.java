d0f72a077111231203884643b493fae75f1bd76d
XarakBendardo <mrcin.wawrzyniakk@gmail.com>
2024-08-13 09:56:15 +0200

    Server and client exchanging data on localhost

--- src/main/java/org/example/Client/Client.java
+++ src/main/java/org/example/Client/Client.java
@@ -1,7 +1,34 @@
 package org.example.Client;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Hello from Client!");
+        Socket socket = null;
+        BufferedWriter out = null;
+        BufferedReader in = null;
+        try {
+            socket = new Socket("localhost", 1234);
+            System.out.println("Connected to server");
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out.write("Hello from client\n");
+            out.flush();
+
+            String response = in.readLine();
+            System.out.println("Received response: " + response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) out.close();
+                if (in != null) in.close();
+                if (socket != null) socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/org/example/Server/Server.java
+++ src/main/java/org/example/Server/Server.java
@@ -1,8 +1,42 @@
 package org.example.Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello from Server!" );
+    private static final int PORT = 1234;
+    public static void main( String[] args ) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        BufferedReader in = null;
+        BufferedWriter out = null;
+
+        try {
+            serverSocket = new ServerSocket(Server.PORT);
+            System.out.println("Server is running on port " + Server.PORT);
+            clientSocket = serverSocket.accept();
+            System.out.println("Client connected");
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            String message = in.readLine();
+            System.out.println("Received message: " + message);
+            out.write("Hello from server\n");
+            out.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) out.close();
+                if (in != null) in.close();
+                if (clientSocket != null) clientSocket.close();
+                if (serverSocket != null) serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
