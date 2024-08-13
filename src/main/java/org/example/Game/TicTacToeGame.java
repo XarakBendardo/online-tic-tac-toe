5de7ff1947f8333a8648454fa6f2c02f268e946e
XarakBendardo <mrcin.wawrzyniakk@gmail.com>
2024-08-13 10:11:53 +0200

    TicTacToeGame and Board classes

---
+++ src/main/java/org/example/Game/TicTacToeGame.java
@@ -0,0 +1,42 @@
+package org.example.Game;
+
+import java.util.Map;
+
+public class TicTacToeGame {
+    public static class Board {
+        public static final int SIZE = 3;
+        public enum Field {X, O, EMPTY}
+        static final Map<Field, Character> map = Map.of(
+                Field.X, 'X',
+                Field.O, 'O',
+                Field.EMPTY, ' '
+        );
+        Field[][] fields;
+
+        public Board() {
+            this.fields = new Field[][]{
+                    {Field.EMPTY, Field.EMPTY, Field.EMPTY},
+                    {Field.EMPTY, Field.EMPTY, Field.EMPTY},
+                    {Field.EMPTY, Field.EMPTY, Field.EMPTY}
+            };
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (var row : fields) {
+                for (var field : row) {
+                    sb.append(map.get(field));
+                }
+                sb.append('\n');
+            }
+            return sb.toString();
+        }
+    }
+
+    private final Board board;
+
+    public TicTacToeGame() {
+        this.board = new Board();
+    }
+}
